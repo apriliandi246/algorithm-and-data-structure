df0256a5a2cbe984e723a20608469c4e1c20091f
apriliandi246 <apriliandi642@gmail.com>
2019-12-28 09:50:44 +0700

    learn queue

---
+++ queue/queue.js
@@ -0,0 +1,54 @@
+// Queue => First In, First Out
+
+class Queue {
+      constructor() {
+            this.collection = [];
+      }
+
+      enqueue(element) {
+            this.collection.push(element);
+      }
+
+      dequeue() {
+            if (this.isEmpty()) {
+                  console.log('Data is empty');
+                  return;
+            }
+
+            this.collection.shift();
+      }
+
+      front() {
+            if (this.isEmpty()) {
+                  console.log('Data is empty');
+                  return;
+            }
+
+            return this.collection[0];
+      }
+
+      isEmpty() {
+            return this.collection.length === 0;
+      }
+
+      printQueue() {
+            let str = "";
+
+            for (let i = 0; i < this.collection.length; i++) {
+                  str += this.collection[i] + " ";
+            }
+
+            return str;
+      }
+}
+
+
+let queue = new Queue();
+
+queue.enqueue(12);
+queue.enqueue(13);
+queue.enqueue(14);
+queue.enqueue(15);
+queue.dequeue();
+
+console.log(queue.printQueue());
