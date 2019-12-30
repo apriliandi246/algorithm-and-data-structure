f10405ae9ca6a832f245b1661cc60ddfa3f943bc
apriliandi246 <apriliandi642@gmail.com>
2019-12-30 11:05:21 +0700

    learn sorth algorith

---
+++ sort_algorith/insert_sort.js
@@ -0,0 +1,22 @@
+function insertSort(arr) {
+      let temp;
+      let pos;
+
+      for (let i = 0; i < arr.length; i++) {
+            temp = arr[i];
+            pos = i;
+
+            for (let j = i; j > 0; j--) {
+                  if (temp < arr[j - 1]) {
+                        arr[j] = arr[j - 1];
+                        pos--;
+                  }
+            }
+
+            arr[pos] = temp;
+      }
+
+      return arr;
+}
+
+console.log(insertSort([10, 9, 8, 7, 5, 6, 3, 4, 1, 2]));
