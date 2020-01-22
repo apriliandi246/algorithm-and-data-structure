0be090c89f5da7cac7d001656a3879d63ee4e292
apriliandi246 <apriliandi642@gmail.com>
2020-01-22 11:15:53 +0700

    add another way to check bracket using stack

---
+++ stack/check_bracket_2.js
@@ -0,0 +1,33 @@
+let data = '[({()})[]}[)';
+let openBracket = [];
+
+if (data.length % 2 === 0) {
+      for (let i = 0; i < data.length; i++) {
+            if (data[i] === '(' || data[i] === '{' || data[i] === '[') {
+                  openBracket.push(data[i]);
+
+            } else if (data[i] === ')' || data[i] === '}' || data[i] === ']') {
+
+                  if (openBracket[openBracket.length - 1] === '(' && data[i] === ')') {
+                        console.log(`${openBracket[openBracket.length - 1]} ${data[i]} = ${true}`);
+                        openBracket.pop();
+
+                  } else if (openBracket[openBracket.length - 1] === '{' && data[i] === '}') {
+                        console.log(`${openBracket[openBracket.length - 1]} ${data[i]} = ${true}`);
+                        openBracket.pop();
+
+                  } else if (openBracket[openBracket.length - 1] === '[' && data[i] === ']') {
+                        console.log(`${openBracket[openBracket.length - 1]} ${data[i]} = ${true}`);
+                        openBracket.pop();
+
+                  } else {
+                        console.log(`${openBracket[openBracket.length - 1]} ${data[i]} = ${false}`);
+                        openBracket.pop();
+                  }
+
+            }
+      }
+
+} else {
+      console.log("--- Format Error ---");
+}
