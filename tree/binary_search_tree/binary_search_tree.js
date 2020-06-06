cfed1864d5d467bc883d3bf10d96403eef235215
apriliandi <apriliandi642@gmail.com>
2020-06-06 13:17:47 +0700

    factory code

--- tree/binary_search_tree/binary_search_tree.js
+++ tree/binary_search_tree/binary_search_tree.js
@@ -1,231 +1,237 @@
 class Node {
-      constructor(data, left = null, right = null) {
-            this.data = data;
-            this.left = left;
-            this.right = right;
-      }
+    constructor(data, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
 }
 
 
 class BST {
-      constructor() {
-            this.root = null;
-      }
+    constructor() {
+        this.root = null;
+    }
 
-      // menambahkan data
-      add(data) {
-            const node = this.root;
+    // menambahkan data
+    add(data) {
+        const node = this.root;
 
+        if (node === null) {
+            this.root = new Node(data);
+            return;
+
+        } else {
+            const searchTree = function (node) {
+                if (data < node.data) {
+                    if (node.left === null) {
+                        node.left = new Node(data);
+                        return;
+
+                    } else {
+                        return searchTree(node.left);
+                    }
+
+                } else if (data > node.data) {
+                    if (node.right === null) {
+                        node.right = new Node(data);
+                        return;
+
+                    } else {
+                        return searchTree(node.right);
+                    }
+
+                } else {
+                    return null;
+                }
+            }
+            searchTree(node);
+        }
+    }
+
+
+    // menghapus data
+    remove(data) {
+        const removeNode = function (node, data) {
             if (node === null) {
-                  this.root = new Node(data);
-                  return;
+                return null;
+            }
+
+            if (data === node.data) {
+                if (node.left === null && node.right === null) {
+                    return null;
+                }
+
+                if (node.left === null) {
+                    return node.right;
+                }
+
+                if (node.right === null) {
+                    return node.left;
+                }
+
+
+                let tempNode = node.right;
+                while (tempNode.left !== null) {
+                    tempNode = tempNode.left;
+                }
+
+                node.data = tempNode.data;
+                node.right = removeNode(node.right, tempNode.data);
+                return node;
+
+            } else if (data < node.data) {
+                node.left = removeNode(node.left, data);
+                return node;
 
             } else {
-                  const searchTree = function (node) {
-                        if (data < node.data) {
-                              if (node.left === null) {
-                                    node.left = new Node(data);
-                                    return;
-
-                              } else {
-                                    return searchTree(node.left);
-                              }
-
-                        } else if (data > node.data) {
-                              if (node.right === null) {
-                                    node.right = new Node(data);
-                                    return;
-
-                              } else {
-                                    return searchTree(node.right);
-                              }
-
-                        } else {
-                              return null;
-                        }
-                  }
-                  searchTree(node);
+                node.right = removeNode(node.right, data);
+                return node;
             }
-      }
-
-      // menghapus data
-      remove(data) {
-            const removeNode = function (node, data) {
-                  if (node === null) {
-                        return null;
-                  }
-
-                  if (data === node.data) {
-                        if (node.left === null && node.right === null) {
-                              return null;
-                        }
-
-                        if (node.left === null) {
-                              return node.right;
-                        }
-
-                        if (node.right === null) {
-                              return node.left;
-                        }
+        }
+        this.root = removeNode(this.root, data);
+    }
 
 
-                        let tempNode = node.right;
-                        while (tempNode.left !== null) {
-                              tempNode = tempNode.left;
-                        }
+    // menemukan data terkecil
+    findMinData() {
+        let current = this.root;
 
-                        node.data = tempNode.data;
-                        node.right = removeNode(node.right, tempNode.data);
-                        return node;
+        while (current.left !== null) {
+            current = current.left;
+        }
 
-                  } else if (data < node.data) {
-                        node.left = removeNode(node.left, data);
-                        return node;
+        return `Min Data => ${current.data}`;
+    }
 
-                  } else {
-                        node.right = removeNode(node.right, data);
-                        return node;
-                  }
-            }
-            this.root = removeNode(this.root, data);
-      }
+    // menemukan data terbesar
+    findMaxData() {
+        let current = this.root;
 
-      // menemukan data terkecil
-      findMinData() {
-            let current = this.root;
+        while (current.right !== null) {
+            current = current.right;
+        }
 
-            while (current.left !== null) {
-                  current = current.left;
-            }
+        return `Max Data => ${current.data}`;
+    }
 
-            return `Min Data => ${current.data}`;
-      }
 
-      // menemukan data terbesar
-      findMaxData() {
-            let current = this.root;
+    // menemukan data
+    findData(data) {
+        let current = this.root;
 
-            while (current.right !== null) {
-                  current = current.right;
-            }
-
-            return `Max Data => ${current.data}`;
-      }
-
-      // menemukan data
-      findData(data) {
-            let current = this.root;
-
-            while (current.data !== data) {
-                  if (data < current.data) {
-                        current = current.left;
-                  } else {
-                        current = current.right;
-                  }
-
-                  if (current === null) {
-                        return null;
-                  }
-            }
-
-            return current;
-      }
-
-      // Step :
-      // - Left, Root, Right
-      inOrder() {
-            if (this.root === null) {
-                  return null;
-
+        while (current.data !== data) {
+            if (data < current.data) {
+                current = current.left;
             } else {
-                  let result = [];
-
-                  function traverseInOrder(node) {
-                        node.left && traverseInOrder(node.left);
-                        result.push(node.data);
-                        node.right && traverseInOrder(node.right);
-                  }
-
-                  traverseInOrder(this.root);
-                  return result;
+                current = current.right;
             }
-      }
 
-
-      // Step :
-      // - Root, Left, Right
-      preOrder() {
-            if (this.root === null) {
-                  return null;
-
-            } else {
-                  let result = [];
-
-                  function traversePreOrder(node) {
-                        result.push(node.data);
-                        node.left && traversePreOrder(node.left);
-                        node.right && traversePreOrder(node.right);
-                  }
-
-                  traversePreOrder(this.root);
-                  return result;
+            if (current === null) {
+                return null;
             }
-      }
+        }
 
-      // Step :
-      // - Left, Right, Root
-      postOrder() {
-            if (this.root === null) {
-                  return null;
+        return current;
+    }
 
-            } else {
-                  let result = [];
 
-                  function traversePostOrder(node) {
-                        node.left && traversePostOrder(node.left);
-                        node.right && traversePostOrder(node.right);
-                        result.push(node.data);
-                  }
+    // Step :
+    // - Left, Root, Right
+    inOrder() {
+        if (this.root === null) {
+            return null;
 
-                  traversePostOrder(this.root);
-                  return result;
-            }
-      }
-
-      // using Queue data structures
-      levelOrder() {
+        } else {
             let result = [];
-            let Q = [];
 
-            if (this.root !== null) {
-                  Q.push(this.root);
-
-                  while (Q.length > 0) {
-                        let node = Q.shift();
-
-                        result.push(node.data);
-
-                        if (node.left !== null) {
-                              Q.push(node.left);
-                        }
-
-                        if (node.right !== null) {
-                              Q.push(node.right);
-                        }
-                  }
-
-                  return result;
-
-            } else {
-                  return null;
+            function traverseInOrder(node) {
+                node.left && traverseInOrder(node.left);
+                result.push(node.data);
+                node.right && traverseInOrder(node.right);
             }
-      }
+
+            traverseInOrder(this.root);
+            return result;
+        }
+    }
 
 
-      // menghapus semua data
-      clearData() {
-            this.root = null;
-      }
+    // Step :
+    // - Root, Left, Right
+    preOrder() {
+        if (this.root === null) {
+            return null;
+
+        } else {
+            let result = [];
+
+            function traversePreOrder(node) {
+                result.push(node.data);
+                node.left && traversePreOrder(node.left);
+                node.right && traversePreOrder(node.right);
+            }
+
+            traversePreOrder(this.root);
+            return result;
+        }
+    }
+
+
+    // Step :
+    // - Left, Right, Root
+    postOrder() {
+        if (this.root === null) {
+            return null;
+
+        } else {
+            let result = [];
+
+            function traversePostOrder(node) {
+                node.left && traversePostOrder(node.left);
+                node.right && traversePostOrder(node.right);
+                result.push(node.data);
+            }
+
+            traversePostOrder(this.root);
+            return result;
+        }
+    }
+
+
+    // using Queue data structures
+    levelOrder() {
+        let result = [];
+        let Q = [];
+
+        if (this.root !== null) {
+            Q.push(this.root);
+
+            while (Q.length > 0) {
+                let node = Q.shift();
+
+                result.push(node.data);
+
+                if (node.left !== null) {
+                    Q.push(node.left);
+                }
+
+                if (node.right !== null) {
+                    Q.push(node.right);
+                }
+            }
+
+            return result;
+
+        } else {
+            return null;
+        }
+    }
+
+
+    // menghapus semua data
+    clearData() {
+        this.root = null;
+    }
 }
 
 
